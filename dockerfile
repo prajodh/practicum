3e78e9630510dce4db5902fcc6de45b084528530
prajodh <prajodhpragaths@gmail.com>
2024-02-08 15:35:12 -0500

    initial commit

---
+++ dockerfile
@@ -0,0 +1,5 @@
+FROM node:latest
+COPY package*.json ./
+RUN npm ci --omit=dev
+COPY . .
+CMD node index.js
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+app.get('/',(req,res) => {
+    res.send('Arithemetic service - Hello World')
+})
+
+app.get('/add/:n/:m', (req, res) => {
+    console.log(req.params.n, req.params.m)
+    res.json(Number(req.params.n) + Number(req.params.m));
+  });
+
+app.listen(port, () => {
+    console.log(`Arithmetic service running at http://localhost:${port}`);
+});
