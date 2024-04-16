92e18b5c6627b28eadc4f84623d5954aba73145a
prajodh <prajodhpragaths@gmail.com>
2024-04-16 16:03:39 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const cors = require('cors');
 
 const {add,sub,mul,div} = require("./arithematica")
